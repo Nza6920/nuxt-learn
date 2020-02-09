841f587d0fddfd339801c8e2b98f401ce2359e22
Nza6920 <1484663282@qq.com>
2020-02-09 18:56:21 +0800

    asyncData && fetch

--- pages/search.vue
+++ pages/search.vue
@@ -1,13 +1,32 @@
 <template>
     <div class="page">
       创建即配置
+      <ul>
+        <li v-for="(item, idx) in list" :key="idx">
+            {{ item }}
+        </li>
+      </ul>
     </div>
 </template>
 
 <script>
+  import axios from 'axios'
   export default {
     name: 'search',
-    layout: 'search'   // 使用模板
+    layout: 'search',   // 使用模板
+    data() {
+      return {
+        list: []
+      }
+    },
+    async asyncData() {  // fetch 处理 vuex , asyncData 处理组件数据
+      let {status, data: {list}} = await axios.get('http://localhost:3000/city/list')
+      if (status === 200) {
+        return {
+          list
+        }
+      }
+    }
   }
 </script>
 
